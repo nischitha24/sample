5e49087ad3bed1e192c15e793b2a12aee02bd9fe
nischitha24 <110897441+nischitha24@users.noreply.github.com>
2023-03-13 09:22:19 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 az ssh vm --port 1224 --name numocity-jump-host --resource-group numocity-jump-host_group 
-az ssh vm --port 1224 --name cms-fusion-test --resource-group numocity-dev-eastus2-rg 
+#az ssh vm --port 1224 --name cms-fusion-test --resource-group numocity-dev-eastus2-rg 
 git clone https://$1@github.com/$2 input-repo --branch $3 --depth 1
 #add tag
 echo "compose location $COMPOSE_LOCATION"
