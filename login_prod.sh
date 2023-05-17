555841d19ee467370f34d19e3a4789dae6aab69a
NischithaSJ <nischitha.sj@numocity.com>
2023-05-17 12:39:32 +0530

    test

--- login_prod.sh
+++ login_prod.sh
@@ -1,6 +1,6 @@
-name_input=$(cat sample/name.json)
-resource_input=$(cat sample/resource.json )
-resource=$(echo "$resource_input" | grep -o '"resource": *"[^"]*"' | awk -F'"' '{print $4}')
+# name_input=$(cat sample/name.json)
+# resource_input=$(cat sample/resource.json )
+# resource=$(echo "$resource_input" | grep -o '"resource": *"[^"]*"' | awk -F'"' '{print $4}')
 
 
-az ssh vm --port 1224 --name $name_input --resource-group $resource
+az ssh vm --port 1224 --name csms-chargepartners-production --resource-group NUMOCITY_PRODUCTION_EASTUS2_RG
