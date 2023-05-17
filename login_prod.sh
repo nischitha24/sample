3e179c7cc4fd91ac2d8e60ee29967564bdfb010f
NischithaSJ <nischitha.sj@numocity.com>
2023-05-17 12:26:04 +0530

    test

--- login_prod.sh
+++ login_prod.sh
@@ -1,4 +1,4 @@
 name_input=$(cat sample/name.json)
-resource_input=$(cat sample/resource.json)
+resource_input=$(cat sample/resource.json | jq -r '.resource')
 
 az ssh vm --port 1224 --name $name_input --resource-group $resource_input
